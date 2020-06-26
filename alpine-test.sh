b778d24c1044b6a697e8d2e45143de1d6f85dd5e
shane <sygibson@gmail.com>
2020-06-26 01:30:29 +0000

    twiddle alpine script a bit

--- alpine-test.sh
+++ alpine-test.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 # fast and dirt virt-install test with Alpine LiveCD
 
+# Usage:
+#
+#  $0 [ pxe | live ] [ <BRIDGE> ] [ <VNC_CONSOLE> ]
+#
+#  "live" is default mode - live boot ISO image
+#  "pxe" attempts to PXE boot on network - make sure Bridge is right
+#  <BRIDGE> defaults to "virbr0" specify alternate
+#  <VNC_CONSOLE> defaults to 127.0.0.1:9999 - specify alternate
+#
+#  BRIDGE is requires if VNC_CONSOLE is specified
+#  ordering of options must be as listed in above
+#
+
 MODE=${1:-"live"}
 NAME="alpine-live-test"
 DESC="Alpine Live Test Virtual Machine"
 BR=${2:-"virbr0"}
+CONS=${3:-"127.0.0.1:9999"}
 
 case $MODE in
   pxe) echo ">>> Setting PXE boot mode for VM."
@@ -26,6 +40,7 @@ ISO="http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-virt-3.12
 virsh desc ${NAME} > /dev/null 2>&1 && { virsh destroy ${NAME}; virsh undefine ${NAME}; }
 
 echo ">>> Creating '${NAME}' virtual machine"
+echo ">>> VNC console listening on $CONS"
 
 set -x
 virt-install \
@@ -38,7 +53,7 @@ virt-install \
 $OPTS \
 --disk /tmp/alpine.iso,device=cdrom,bus=ide \
 --network bridge=virbr0,model=e1000 \
---graphics vnc,listen=0.0.0.0,password=foobar --check all=off
+--graphics vnc,listen=${CONS},password=foobar --check all=off
 set +x
 
 echo ""
